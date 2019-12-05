5187169741f1ecfdbe7e4f8c953077d90415dfa2
Zeshan Raja <zraja4402@gmail.com>
2019-12-05 23:53:22 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,20 +15,6 @@ def load_library(yaml)
 
 translator_hash 
 end  
-# pp emoticon_hash = 
-#{"angel"=>["O:)", "☜(⌒▽⌒)☞"], "
-#  angry"=>[">:(", "ヽ(ｏ`皿′ｏ)ﾉ"],
-# "bored"=>[":O", "(ΘεΘ;)"],
-# "confused"=>["%)", "(゜.゜)"], 
-#"embarrased"=>[":$", "(#^.^#)"],
-# "fish"=>["><>", ">゜))))彡"],
-# "glasses"=>["8D", "(^0_0^)"],
-# "grinning"=>["=D", "(￣ー￣)"],
-# "happy"=>[":)", "(＾ｖ＾)"],
-# "kiss"=>[":*", "(*^3^)/~☆"],
-# "sad"=>[":'(", "(Ｔ▽Ｔ)"],
-# "surprised"=>[":o", "o_O"],
-# "wink"=>[";)", "(^_-)"]}
 
 def get_japanese_emoticon(path, emoticon)
   hash = load_library(path)
